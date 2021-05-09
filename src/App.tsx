7b0ab73a2359adf8e7a66108ea21fe204e06037b
rao <68511759+raorao1011@users.noreply.github.com>
2021-05-09 20:05:17 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./router/Router";
+
+import theme from "./theme/theme";
+
+export default function App() {
+  return (
+    <div className="App">
+      <ChakraProvider theme={theme}>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+      </ChakraProvider>
+    </div>
+  );
+}
