c89ee68bfee5b9f2f7e22308fa398d94f0480e84
rao <68511759+raorao1011@users.noreply.github.com>
2021-05-14 18:13:23 +0900

    add UserCard Component

--- src/components/pages/UserManegement.tsx
+++ src/components/pages/UserManegement.tsx
@@ -1,5 +1,18 @@
+import { Wrap, WrapItem } from "@chakra-ui/react";
 import { memo, VFC } from "react";
 
+import { UserCard } from "../organism/user/UserCard";
+
 export const UserManegement: VFC = memo(() => {
-  return <p>ユーザー管理ページです</p>;
+  return (
+    <Wrap p={{ base: 4, md: 10 }}>
+      <WrapItem>
+        <UserCard
+          imageUrl="https:/source.unsplash.com/random"
+          userName="じゃけぇ"
+          fullName="Takumi Okada"
+        />
+      </WrapItem>
+    </Wrap>
+  );
 });
