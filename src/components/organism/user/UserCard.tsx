e648287d993a957c3b94ea3353da29892e21b1f2
rao <68511759+raorao1011@users.noreply.github.com>
2021-05-15 00:26:11 +0900

    add new cusHook and add viewAllUser

--- src/components/organism/user/UserCard.tsx
+++ src/components/organism/user/UserCard.tsx
@@ -1,15 +1,15 @@
 import { Box, Image, Stack, Text } from "@chakra-ui/react";
-import { memo, VFC } from "react";
+import { VFC } from "react";
 
 type Props = {
   imageUrl: string;
   userName: string;
   fullName: string;
+  onClick: () => void;
 };
 
-export const UserCard: VFC<Props> = memo((props) => {
-  const { imageUrl, userName, fullName } = props;
-  console.log(imageUrl);
+export const UserCard: VFC<Props> = (props) => {
+  const { imageUrl, userName, fullName, onClick } = props;
   return (
     <Box
       w="260px"
@@ -19,6 +19,7 @@ export const UserCard: VFC<Props> = memo((props) => {
       shadow="md"
       p={4}
       _hover={{ cursor: "pointer", opacity: 0.8 }}
+      onClick={onClick}
     >
       <Stack textAlign="center">
         <Image
@@ -37,4 +38,4 @@ export const UserCard: VFC<Props> = memo((props) => {
       </Stack>
     </Box>
   );
-});
+};
--- src/components/pages/UserManegement.tsx
+++ src/components/pages/UserManegement.tsx
@@ -1,18 +1,53 @@
-import { Wrap, WrapItem } from "@chakra-ui/react";
-import { memo, VFC } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import {
+  Center,
+  Modal,
+  ModalContent,
+  ModalOverlay,
+  Spinner,
+  useDisclosure,
+  Wrap,
+  WrapItem
+} from "@chakra-ui/react";
+import { memo, useCallback, useEffect, VFC } from "react";
 
 import { UserCard } from "../organism/user/UserCard";
+import { useAllUsers } from "../../hooks/useAllUsers";
 
 export const UserManegement: VFC = memo(() => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { getUsers, users, loading } = useAllUsers();
+
+  useEffect(() => getUsers(), []);
+
+  const onClickUser = useCallback(() => onOpen(), []);
+
   return (
-    <Wrap p={{ base: 4, md: 10 }}>
-      <WrapItem>
-        <UserCard
-          imageUrl="https:/source.unsplash.com/random"
-          userName="じゃけぇ"
-          fullName="Takumi Okada"
-        />
-      </WrapItem>
-    </Wrap>
+    <>
+      {loading ? (
+        <Center height="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }}>
+          {users?.map((user) => (
+            <WrapItem key={user.id} mx="auto">
+              <UserCard
+                imageUrl={`https://source.unsplash.com/random/user`}
+                userName={user.username}
+                fullName={user.name}
+                onClick={onClickUser}
+              />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <p>テスト</p>
+        </ModalContent>
+      </Modal>
+    </>
   );
 });
